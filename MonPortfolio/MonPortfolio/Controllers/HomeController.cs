66500747b7137fb39f6a028a4ae4f5780901f79d
ahoua elvis ghislain <59898872+ahoua900@users.noreply.github.com>
2022-10-12 18:32:46 +0000

    commit

---
+++ MonPortfolio/MonPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return PartialView();
+        }
+        public ActionResult Services()
+        {
+            return PartialView();
+        }
+        public ActionResult Resume()
+        {
+            return PartialView();
+        }
+        public ActionResult Skills()
+        {
+            return PartialView();
+        }
+        public ActionResult Portfolio()
+        {
+            return PartialView();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return PartialView();
+        }
+    }
+}
